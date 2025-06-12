79f51a7ffa571936e7bae988fbd4a1566991d89a
hyliansyrup <100920497+hyliansyrup@users.noreply.github.com>
2025-06-12 20:07:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -44,4 +44,6 @@ def static_files(path):
     return send_from_directory("static", path)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
+
